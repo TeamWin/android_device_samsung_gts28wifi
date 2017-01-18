1129f541432bb3d65a621990ec353a52c4eafcd7
LuK1337 <priv.luk@gmail.com>
2017-01-19 00:49:09 +0100

    gts210vewifi: Fix kernel base

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ TARGET_PREBUILT_KERNEL := device/samsung/gts210vewifi/kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/gts210vewifi/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x80000000 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --board FPRPG21W000KU --dt device/samsung/gts210vewifi/dt.img
+BOARD_MKBOOTIMG_ARGS := --base 0x80000000 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --board FPRPG21W000KU --dt device/samsung/gts210vewifi/dt.img
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
