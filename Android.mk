b50592d55d0948b7949094ccb15bea11f126a7bb
ripee <ripee.dev@gmail.com>
2017-12-06 15:34:36 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter trlte,$(TARGET_DEVICE)),)
+ifneq ($(filter gts210wifi gts210wifixx,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
