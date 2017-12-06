a24cbc69560aee8ff677e97947ecdc8c9d7b816e
ripee <ripee.dev@gmail.com>
2017-12-06 15:47:27 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,15 +1,15 @@
-DEVICE_TREE := device/samsung/trlte
+DEVICE_TREE := device/samsung/gts210wifi
 
 CM_PLATFORM_SDK_VERSION := 7	# Required for libf2fs.so
 override TARGET_OUT_VENDOR_SHARED_LIBRARIES = $(TARGET_OUT_SHARED_LIBRARIES)
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := APQ8084
+TARGET_BOOTLOADER_BOARD_NAME := universal5433
 
 # Platform
-TARGET_BOARD_PLATFORM := apq8084
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno420
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_BOARD_PLATFORM_GPU := mali-t760mp6
 
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
@@ -18,34 +18,29 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/apq8084
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_CONFIG := twrp_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_eur_defconfig
-KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_TREE)/dt.img
 
 # Boot image
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02600000 --tags_offset 0x02400000 --board SYSMAGIC000RU
+BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+# 000RU = recovery kernel, 000KU = system kernel
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000RU
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/mkbootimg.mk
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x001100000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x001300000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0E1000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x62EFF7000 #0x62EFFB000 - 16384 (footer)
-BOARD_FLASH_BLOCK_SIZE := 0x40000
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x105C0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00D00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x105C0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105C0000
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -54,33 +49,27 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
 # TWRP specific build flags
-TW_THEME := portrait_hdpi
+TW_THEME := landscape_hdpi
 RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/f9200000.ssusb/f9200000.dwc3/gadget/lun%d/file"
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/15400000.usb/15400000.dwc3/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/13800000.decon_fb/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_NTFS_3G := true
-# exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
-# No love for the wicked (device ships with M)
+TW_MTP_DEVICE := "/dev/mtp_usb"
 TW_EXCLUDE_SUPERSU := true
+
+# Touchscreen correction
+RECOVERY_TOUCHSCREEN_SWAP_XY := true
+RECOVERY_TOUCHSCREEN_FLIP_Y := true
 
 # Encryption support
 TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
-TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
-# Debug flags
-# TWRP_INCLUDE_LOGCAT := true
 
 # Init properties from bootloader version, ex. model info
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_trlte
-TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_trlte.cpp
-
+TARGET_INIT_VENDOR_LIB := libinit_gts210wifi
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_gts210wifi.cpp
