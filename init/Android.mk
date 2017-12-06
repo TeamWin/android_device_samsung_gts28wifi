a213f974c63286fa437325933a7d3f1d293f6d37
ripee <ripee.dev@gmail.com>
2017-12-06 16:38:00 -0500

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_gts210wifi.cpp
-LOCAL_MODULE := libinit_gts210wifi
+LOCAL_SRC_FILES := init_gts28wifi.cpp
+LOCAL_MODULE := libinit_gts28wifi
 
 include $(BUILD_STATIC_LIBRARY)
